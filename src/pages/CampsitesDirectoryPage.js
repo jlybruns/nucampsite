24206049279ece1c97acef738f6824a3aac9d3ae
Joel Bruns <jlybruns@gmail.com>
2024-03-06 07:19:15 -0500

    Added onclick to the the campsite cards

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,16 +1,18 @@
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col, } from 'reactstrap';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+import { selectCampsiteById } from '../features/campsites/campsitesSlice';
+import { useState } from  'react';
 
 const CampsitesDirectoryPage = () => {
-    const selectedCampsite = selectRandomCampsite();
-
+    const [ campsiteId, setCampsiteId ] = useState(0);
+    const selectedCampsite = selectCampsiteById(campsiteId);
     return (
         <Container>
+            
             <Row>
                 <Col sm='5' md='7'>
-                    <CampsitesList />
+                    <CampsitesList setCampsiteId={setCampsiteId} />
                 </Col>
                 <Col sm='7' md='5'>
                     <CampsiteDetail campsite = {selectedCampsite} />
